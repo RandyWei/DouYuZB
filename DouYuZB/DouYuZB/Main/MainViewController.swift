e2167cc08d69a63589c9da5dffeefc11ed98d662
RandyWei <smile561607154@163.com>
2019-09-07 13:42:31 +0800

    设置Tab图标

--- DouYuZB/DouYuZB/Main/MainViewController.swift
+++ DouYuZB/DouYuZB/Main/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
     
